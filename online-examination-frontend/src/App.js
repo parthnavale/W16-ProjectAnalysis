b88e81cd120c62311bdb0d0d3079773111ddc2b5
Meet Patel <patel00446@gmail.com>
2021-02-24 14:13:38 -0500

    Added routing for navigation

--- online-examination-frontend/src/App.js
+++ online-examination-frontend/src/App.js
@@ -3,13 +3,20 @@ import Footer from "./components/Footer/Footer";
 import Main from "./components/Main/Main";
 import Login from "./components/Login/Login.js";
 import Register from "./components/Register/Register";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Main />
-      <Footer />
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
