8fdc2021e140dbcc3792cfee07cb9e839f5f6af5
Vatsal Anilkumar Chauhan <vchauhan@isimtl.local>
2021-03-03 18:35:46 -0500

    Updated Package json and Errors

--- online-examination-frontend/src/components/Header/Header.js
+++ online-examination-frontend/src/components/Header/Header.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { Navbar, Nav} from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 import "./Header.css";
-import {LinkContainer} from 'react-router-bootstrap';
-import {AuthContext} from "../../App";
+import { LinkContainer } from 'react-router-bootstrap';
+import { AuthContext } from "../../App";
 
 class Header extends React.Component {
   constructor(props) {
@@ -12,7 +12,7 @@ class Header extends React.Component {
       isClickedLogin: true
     };
   }
-  
+
   checkIfLoginOrNot() {
     let isLoggedIn = false;
     try {
@@ -30,67 +30,68 @@ class Header extends React.Component {
     return isLoggedIn;
   }
 
-  render(){ 
+  render() {
     return (
-    <Navbar
-      collapseOnSelect
-      className="header-main-navbar"
-      expand="lg"
-      variant="light"
-    >
-      <div className="row col-md-12">
-        <div className="col-md-10 col-sm-12 col-xs-12 col-lg-4 float-left">
-       
-        <LinkContainer to="/">
-          <Navbar.Brand className="header-main-heading" href="#home">
-            <p>Online Examination System</p>
-          </Navbar.Brand>
-          </LinkContainer>
-        </div>
-        <div className="col-md-2 col-sm-12 col-xs-12 col-lg-8 float-right">
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav" className="float-right">
-            <LinkContainer to="/scores">
-              <Nav.Link>
-                <h4 className="linkText">Test-Takers</h4>
-              </Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/taketest">
-            <Nav.Link>
-              <h4 className="linkText">Institutions</h4>
-            </Nav.Link>
+      <Navbar
+        collapseOnSelect
+        className="header-main-navbar"
+        expand="lg"
+        variant="light"
+      >
+        <div className="row col-md-12">
+          <div className="col-md-10 col-sm-12 col-xs-12 col-lg-4 float-left">
+
+            <LinkContainer to="/">
+              <Navbar.Brand className="header-main-heading" href="#home">
+                <p>Online Examination System</p>
+              </Navbar.Brand>
             </LinkContainer>
-            { this.checkIfLoginOrNot() && (
-            <AuthContext.Consumer>
-            {context => (
-             <LinkContainer to='/'>
-             <Nav.Link>
-               <h4 className="linkText" onClick={() => {
+          </div>
+          <div className="col-md-2 col-sm-12 col-xs-12 col-lg-8 float-right">
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav" className="float-right">
+              <LinkContainer to="/scores">
+                <Nav.Link>
+                  <h4 className="linkText">Test-Takers</h4>
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/institution">
+                <Nav.Link>
+                  <h4 className="linkText">Institutions</h4>
+                </Nav.Link>
+              </LinkContainer>
+              {this.checkIfLoginOrNot() && (
+                <AuthContext.Consumer>
+                  {context => (
+                    <LinkContainer to='/'>
+                      <Nav.Link>
+                        <h4 className="linkText" onClick={() => {
                           context.clearAuth();
                         }}>Logout</h4>
-             </Nav.Link>
-             </LinkContainer>
+                      </Nav.Link>
+                    </LinkContainer>
+                  )}
+                </AuthContext.Consumer>
               )}
-              </AuthContext.Consumer>
-            )}
-        { !this.checkIfLoginOrNot() && (<>
-          <LinkContainer to="/login">
-            <Nav.Link>
-              <h4 className="linkText">Login</h4>
-            </Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/register">
-            <Nav.Link>
-              <h4 className="linkText">SignUp</h4>
-            </Nav.Link>
-            </LinkContainer>
-          </>
-        )}
-          </Navbar.Collapse>
+              {!this.checkIfLoginOrNot() && (<>
+                <LinkContainer to="/login">
+                  <Nav.Link>
+                    <h4 className="linkText">Login</h4>
+                  </Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/register">
+                  <Nav.Link>
+                    <h4 className="linkText">SignUp</h4>
+                  </Nav.Link>
+                </LinkContainer>
+              </>
+              )}
+            </Navbar.Collapse>
+          </div>
         </div>
-      </div>
-    </Navbar>
-  )};
+      </Navbar>
+    )
+  };
 };
 
 export default Header;
