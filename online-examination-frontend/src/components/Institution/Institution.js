52e476372b138fa10062de2b8b9466f2b52f1fc7
Vatsal Anilkumar Chauhan <vchauhan@isimtl.local>
2021-03-03 19:04:57 -0500

    Added Institution list Page

--- online-examination-frontend/src/components/Institution/Institution.js
+++ online-examination-frontend/src/components/Institution/Institution.js
@@ -1,76 +1,89 @@
-import React, {Component} from "react";
+import React from "react";
 import "./Institution.css";
+
 
 import "bootstrap/dist/css/bootstrap.css";
 import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
 
 import BootstrapTable from "react-bootstrap-table-next";
 import paginationFactory from "react-bootstrap-table2-paginator";
-import $ from 'jquery';
 
+
+const initialState = {
+    institutionList: []
+};
 export default class Institution extends React.Component {
     constructor(props) {
-      super(props);
-      this.getHeader = this.getHeader.bind(this);
-      this.getRowsData = this.getRowsData.bind(this);
-      this.getKeys = this.getKeys.bind(this);
-      this.state = {data: 'false'};
+        super(props);
+        this.state = { ...initialState }
     }
+
     componentDidMount() {
         this._getData();
     }
     _getData = () => {
-        fetch("http://localhost:8000/institutions")
-        .then(response => {
-            if (response.ok) {
+        fetch("http://localhost:8000/api/institutions/listOfInstitutions")
+            .then(response => {
+                if (response.ok) {
                     return response;
-            } else {
-                let errorMessage = 'error';
-                console.log(errorMessage);
-            }
-        })
-        .then(response => response.json())
-        .then(json =>{
-           console.log(json);
-           this.setState({ data: json.data })
-        });
+                } else {
+                    let errorMessage = 'error';
+                    console.log(errorMessage);
+                }
+            })
+            .then(response => response.json())
+            .then(json => {
+                this.setState({ institutionList: json })
+            });
     }
-    getKeys = function(){
-        return Object.keys(this.props.data[0]);
-    }
-    
-    getHeader = function(){
-        var keys = this.getKeys();
-        return keys.map((key, index)=>{
-        return <th key={key}>{key.toUpperCase()}</th>
-        })
-    }
-    
-    getRowsData = function(){
-        var items = this.props.data;
-        var keys = this.getKeys();
-        return items.map((row, index)=>{
-        return <tr key={index}><RenderRow key={index} data={row} keys={keys}/></tr>
-        })
-    }
-    render() {
-        return (
-        <div>
-        <table data={this.state.data}>
-        <thead>
-        <tr>{this.getHeader()}</tr>
-        </thead>
-        <tbody>
-        {this.getRowsData()}
-        </tbody>
-        </table>
-        </div>
-        
-        );
+
+    productsGenerator() {
+        let items = [];
+        const { institutionList } = this.state;
+        if (institutionList) {
+            institutionList.forEach((item, index) => {
+                items.push({
+                    instituteId: item.instituteId,
+                    institutionName: item.institutionName,
+                    institutionLocation: item.institutionLocation
+                });
+            })
         }
-  }
-  const RenderRow = (props) =>{
-    return props.keys.map((key, index)=>{
-    return <td key={props.data[key]}>{props.data[key]}</td>
-    })
-   }
+        return items;
+    }
+
+
+
+    columnNames() {
+        const columns = [
+            {
+                dataField: "institutionName",
+                text: "Institute Name",
+                sort: true
+            },
+            {
+                dataField: "institutionLocation",
+                text: "Institution Location"
+            }
+        ];
+        return columns;
+    }
+
+    render() {
+        const productList = this.productsGenerator() || [];
+        return (
+            <React.Fragment>
+                <br />
+                <h1 className="display-4" >List of institutions</h1>
+                <p className="paragraph1">This are the list of institutions that accepts out test scores.</p>
+                <BootstrapTable
+                    bootstrap4
+                    keyField="instituteId"
+                    data={productList}
+                    columns={this.columnNames()}
+                    pagination={paginationFactory({ sizePerPage: 5 })}
+                />
+            </React.Fragment>
+        );
+    }
+}
--- online-examination-frontend/src/components/Taketest/Taketest.js
+++ online-examination-frontend/src/components/Taketest/Taketest.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { Jumbotron,Container,Image,Row,Col, Button } from 'react-bootstrap';
+import { Jumbotron, Container, Image, Row, Button } from 'react-bootstrap';
 import './Taketest.css';
-import {LinkContainer} from 'react-router-bootstrap';
 
-class Taketest extends React.Component{
+class Taketest extends React.Component {
 
     render() {
         return (
@@ -11,45 +10,45 @@ class Taketest extends React.Component{
                 <Jumbotron className="design-layout">
                     <Container>
                         <Row>
-                    <Image src="icon1.png"/>{' '}
-                        <h1>Fluid jumbotron</h1>
+                            <Image src="icon1.png" />{' '}
+                            <h1>Fluid jumbotron</h1>
                         </Row >
                         <Row className="justify-content-between">
-                        <p>
-                        This is a modified jumbotron that occupies the entire horizontal space of
-                        its parent.
+                            <p>
+                                This is a modified jumbotron that occupies the entire horizontal space of
+                                its parent.
                         </p>
-                        <Button>Take Test</Button>
+                            <Button>Take Test</Button>
                         </Row>
                     </Container>
                 </Jumbotron>
                 <Jumbotron className="design-layout">
                     <Container>
-                    <Row>
-                    <Image src="icon1.png"/>{' '}
-                        <h1>Fluid jumbotron</h1>
+                        <Row>
+                            <Image src="icon1.png" />{' '}
+                            <h1>Fluid jumbotron</h1>
                         </Row >
                         <Row className="justify-content-between">
-                        <p>
-                        This is a modified jumbotron that occupies the entire horizontal space of
-                        its parent.
+                            <p>
+                                This is a modified jumbotron that occupies the entire horizontal space of
+                                its parent.
                         </p>
-                        <Button>Take Test</Button>
+                            <Button>Take Test</Button>
                         </Row>
                     </Container>
                 </Jumbotron>
                 <Jumbotron className="design-layout">
                     <Container>
-                         <Row>
-                    <Image src="icon1.png" className="mb-2"/>{' '}
-                        <h1>Fluid jumbotron</h1>
+                        <Row>
+                            <Image src="icon1.png" className="mb-2" />{' '}
+                            <h1>Fluid jumbotron</h1>
                         </Row >
                         <Row className="justify-content-between">
-                        <p>
-                        This is a modified jumbotron that occupies the entire horizontal space of
-                        its parent.
+                            <p>
+                                This is a modified jumbotron that occupies the entire horizontal space of
+                                its parent.
                         </p>
-                        <Button>Take Test</Button>
+                            <Button>Take Test</Button>
                         </Row>
                     </Container>
                 </Jumbotron>
